f65b4ecd18c366120bf7654bd13a09cfe81ed004
Ivan Wu <bocksheng961136@gmail.com>
2023-07-07 14:21:41 +0800

    check file exist or not

--- products.py
+++ products.py
@@ -1,14 +1,21 @@
-#read file
+import os #operation system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if 'name,price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
 
+#read file
+if os.path.isfile('products.csv'):
+	print('There is the file.')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'name,price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('There is not that file.')
 
+#user input
 while True:
 	name = input('plese enter the product name:')
 	if name == 'q':
@@ -18,9 +25,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+#print record
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#write file
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('name,price\n')
 	for p in products:
