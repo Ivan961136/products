8a10fc985d4f3d2d72872e19f78a60045088a81b
Ivan Wu <bocksheng961136@gmail.com>
2023-07-07 13:53:05 +0800

    read file

--- products.py
+++ products.py
@@ -1,8 +1,27 @@
+#read file
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'name,price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+
 while True:
 	name = input('plese enter the product name:')
 	if name == 'q':
 		break
 	price = input('plese enter the product price:')
+	price = int(price)
 	products.append([name, price])
 print(products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('name,price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
